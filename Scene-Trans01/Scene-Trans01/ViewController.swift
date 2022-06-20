bceb917cef7603db14d4ffe8d819b0158fe30794
이두산 <sxfc675@gmail.com>
2022-06-20 21:36:54 +0900

    [22.06.20] IOS 에서 화면전환하는 방법에 대한 프로젝트 생성

---
+++ Scene-Trans01/Scene-Trans01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scene-Trans01
+//
+//  Created by 이두산 on 2022/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
